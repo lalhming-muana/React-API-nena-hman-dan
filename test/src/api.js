633ba3c5cb9837f25b4f80045624d6dab34f4144
Muana <lalhming.muana1982@gmail.com>
2023-05-30 21:14:50 +0530

    A new commit till data fetching cleanup

---
+++ test/src/api.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const searchImages= async(term)=>{
+
+const response = await axios.get('https://api.unsplash.com/search/photos',{
+    headers: {
+      Authorization: process.env.REACT_APP_API_KEY,
+    },
+
+    params: {
+      query: term,
+    },
+  });
+
+ return response.data.results;
+
+};
+
+export default searchImages;
